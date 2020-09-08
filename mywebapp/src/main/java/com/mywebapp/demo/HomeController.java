619860ad4a496672f764e5290d0c71ca9dcc048b
sureshgithub1 <mishra.suresh888@gmail.com>
2020-09-09 01:55:43 +0530

    first spring boot webapp

---
+++ mywebapp/src/main/java/com/mywebapp/demo/HomeController.java
@@ -0,0 +1,17 @@
+package com.mywebapp.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("home")
+	public String home() {
+		
+		return "home.jsp";
+	}
+	
+	
+
+}
---
+++ mywebapp/src/test/java/com/mywebapp/demo/MywebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywebapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
